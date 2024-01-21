9630dc26e6f56dcab76efeb9f68114f6e662d819
vinayaknayar <nayarvinayak03@gmail.com>
2024-01-22 02:27:50 +0530

    feat: connection to DB

--- configs/app.go
+++ configs/app.go
@@ -1,13 +1,46 @@
 package configs
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/gorm"
-	_ "gorm.io/driver/mysql"
+	"gorm.io/driver/mysql"
 )
 
 var (
 	db * gorm.DB
 )
 
-func Connect() {
+// connect to database
+func Connect() (*gorm.DB, error) {
+	//Load  environment variables
+	err := godotenv.Load(".env")
+	if err != nil {
+		return nil, err
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	// create connection string
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	// initailize the mysql session
+	d, err := gorm.Open(mysql.Open(connection), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	db = d
+	return db, nil
+}
+
+// get database
+func GetDB() *gorm.DB {
+	return db
 }
